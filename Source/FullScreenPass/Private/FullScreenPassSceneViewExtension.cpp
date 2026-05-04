11eab44b0a6cffa2150bef9113a4a2041ee81899
darknesod <darknesod1@gmail.com>
2026-05-04 20:33:03 +0300

    Plugin Works

--- Source/FullScreenPass/Private/FullScreenPassSceneViewExtension.cpp
+++ Source/FullScreenPass/Private/FullScreenPassSceneViewExtension.cpp
@@ -7,74 +7,96 @@
 
 
 static TAutoConsoleVariable<int32> CVarEnabled(
-	TEXT("r.FSP"),
-	1,
-	TEXT("Controls Full Screen Pass plugin\n")
-	TEXT(" 0: disabled\n")
-	TEXT(" 1: enabled (default)"));
+    TEXT("r.FSP"),
+    1,
+    TEXT("Controls Full Screen Pass plugin\n")
+    TEXT(" 0: disabled\n")
+    TEXT(" 1: enabled (default)"));
 
-static TAutoConsoleVariable<float> CVarEffectStrenght(
-	TEXT("r.FSP.EffectStrenght"),
-	0.1f,
-	TEXT("Controls an amount of depth buffer blending to the base color."));
+static TAutoConsoleVariable<float> CVarSharpStrength(
+    TEXT("r.FSP.SharpStrength"),
+    0.7f,
+    TEXT("Sharpen Strength (0.0 - 1.0)"));
+
+static TAutoConsoleVariable<int32> CVarDepthMaskEnable(
+    TEXT("r.FSP.DepthMaskEnable"),
+    1,
+    TEXT("Use Depth Mask\n")
+    TEXT(" 0: disabled\n")
+    TEXT(" 1: enabled (default)"));
+
+static TAutoConsoleVariable<int32> CVarRmsMaskEnable(
+    TEXT("r.FSP.RmsMaskEnable"),
+    1,
+    TEXT("Use Local Contrast Enhancer (RMS Mask)\n")
+    TEXT(" 0: disabled\n")
+    TEXT(" 1: enabled (default)"));
+
+static TAutoConsoleVariable<int32> CVarSharpenMode(
+    TEXT("r.FSP.SharpenMode"),
+    1,
+    TEXT("Sharpen Mode\n")
+    TEXT(" 0: Chroma\n")
+    TEXT(" 1: Luma (default)"));
 
 
 FFullScreenPassSceneViewExtension::FFullScreenPassSceneViewExtension(const FAutoRegister& AutoRegister) :
-	FSceneViewExtensionBase(AutoRegister)
+    FSceneViewExtensionBase(AutoRegister)
 {
 
 }
 
 void FFullScreenPassSceneViewExtension::PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& View, const FPostProcessingInputs& Inputs)
 {
-	if (CVarEnabled->GetInt() == 0)
-	{
-		return;
-	}
+    if (CVarEnabled->GetInt() == 0)
+    {
+        return;
+    }
 
-	Inputs.Validate();
+    Inputs.Validate();
 
-	const FIntRect PrimaryViewRect = UE::FXRenderingUtils::GetRawViewRectUnsafe(View);
+    const FIntRect PrimaryViewRect = UE::FXRenderingUtils::GetRawViewRectUnsafe(View);
 
-	FScreenPassTexture SceneColor((*Inputs.SceneTextures)->SceneColorTexture, PrimaryViewRect);
+    FScreenPassTexture SceneColor((*Inputs.SceneTextures)->SceneColorTexture, PrimaryViewRect);
 
-	if (!SceneColor.IsValid())
-	{
-		return;
-	}
+    if (!SceneColor.IsValid())
+    {
+        return;
+    }
 
-	const FScreenPassTextureViewport Viewport(SceneColor);
+    const FScreenPassTextureViewport Viewport(SceneColor);
 
-	FRDGTextureDesc SceneColorDesc = SceneColor.Texture->Desc;
+    FRDGTextureDesc SceneColorDesc = SceneColor.Texture->Desc;
+    SceneColorDesc.Format = PF_FloatRGBA;
+    FLinearColor ClearColor(0., 0., 0., 0.);
+    SceneColorDesc.ClearValue = FClearValueBinding(ClearColor);
 
-	SceneColorDesc.Format = PF_FloatRGBA;
-	FLinearColor ClearColor(0., 0., 0., 0.);
-	SceneColorDesc.ClearValue = FClearValueBinding(ClearColor);
+    FRDGTexture* ResultTexture = GraphBuilder.CreateTexture(SceneColorDesc, TEXT("FullScreenPassResult"));
+    FScreenPassRenderTarget ResultRenderTarget = FScreenPassRenderTarget(ResultTexture, SceneColor.ViewRect, ERenderTargetLoadAction::EClear);
 
-	FRDGTexture* ResultTexture = GraphBuilder.CreateTexture(SceneColorDesc, TEXT("FulllScreenPassResult"));
-	FScreenPassRenderTarget ResultRenderTarget = FScreenPassRenderTarget(ResultTexture, SceneColor.ViewRect, ERenderTargetLoadAction::EClear);
-	
-	FGlobalShaderMap* GlobalShaderMap = GetGlobalShaderMap(GMaxRHIFeatureLevel);
-	TShaderMapRef<FFullScreenPassVS> ScreenPassVS(GlobalShaderMap);
-	TShaderMapRef<FFullScreenPassPS> ScreenPassPS(GlobalShaderMap);
+    FGlobalShaderMap* GlobalShaderMap = GetGlobalShaderMap(GMaxRHIFeatureLevel);
+    TShaderMapRef<FFullScreenPassVS> ScreenPassVS(GlobalShaderMap);
+    TShaderMapRef<FFullScreenPassPS> ScreenPassPS(GlobalShaderMap);
 
-	FFullScreenPassPS::FParameters* Parameters = GraphBuilder.AllocParameters<FFullScreenPassPS::FParameters>();
-	Parameters->View = View.ViewUniformBuffer;
-	Parameters->SceneTexturesStruct = Inputs.SceneTextures;
-	Parameters->Strenght = CVarEffectStrenght->GetFloat();
+    FFullScreenPassPS::FParameters* Parameters = GraphBuilder.AllocParameters<FFullScreenPassPS::FParameters>();
+    Parameters->View                = View.ViewUniformBuffer;
+    Parameters->SceneTexturesStruct = Inputs.SceneTextures;
+    Parameters->SharpStrength       = CVarSharpStrength->GetFloat();
+    Parameters->DepthMaskEnable     = CVarDepthMaskEnable->GetInt();
+    Parameters->RmsMaskEnable       = CVarRmsMaskEnable->GetInt();
+    Parameters->SharpenMode         = CVarSharpenMode->GetInt();
+    Parameters->RenderTargets[0]    = ResultRenderTarget.GetRenderTargetBinding();
 
-	Parameters->RenderTargets[0] = ResultRenderTarget.GetRenderTargetBinding();
+    AddDrawScreenPass(
+        GraphBuilder,
+        RDG_EVENT_NAME("FullScreenPassShader"),
+        View,
+        Viewport,
+        Viewport,
+        ScreenPassVS,
+        ScreenPassPS,
+        Parameters
+    );
 
-	AddDrawScreenPass(
-		GraphBuilder,
-		RDG_EVENT_NAME("FullScreenPassShader"),
-		View,
-		Viewport,
-		Viewport,
-		ScreenPassVS,
-		ScreenPassPS,
-		Parameters
-	);
-
-	AddCopyTexturePass(GraphBuilder, ResultTexture, SceneColor.Texture);
+    AddCopyTexturePass(GraphBuilder, ResultTexture, SceneColor.Texture);
 }
--- Source/FullScreenPass/Public/FullScreenPassShaders.h
+++ Source/FullScreenPass/Public/FullScreenPassShaders.h
@@ -7,21 +7,24 @@
 class FFullScreenPassVS : public FGlobalShader
 {
 public:
-	DECLARE_GLOBAL_SHADER(FFullScreenPassVS);
+    DECLARE_GLOBAL_SHADER(FFullScreenPassVS);
 
-	FFullScreenPassVS() = default;
-	FFullScreenPassVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer) : FGlobalShader(Initializer) {}
+    FFullScreenPassVS() = default;
+    FFullScreenPassVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer) : FGlobalShader(Initializer) {}
 };
 
 class FFullScreenPassPS : public FGlobalShader
 {
-	DECLARE_GLOBAL_SHADER(FFullScreenPassPS);
-	SHADER_USE_PARAMETER_STRUCT(FFullScreenPassPS, FGlobalShader);
+    DECLARE_GLOBAL_SHADER(FFullScreenPassPS);
+    SHADER_USE_PARAMETER_STRUCT(FFullScreenPassPS, FGlobalShader);
 
-	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
-		RENDER_TARGET_BINDING_SLOTS()
-		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
-		SHADER_PARAMETER_RDG_UNIFORM_BUFFER(FSceneTextureUniformParameters, SceneTexturesStruct)
-		SHADER_PARAMETER(float, Strenght)
-	END_SHADER_PARAMETER_STRUCT()
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        RENDER_TARGET_BINDING_SLOTS()
+        SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
+        SHADER_PARAMETER_RDG_UNIFORM_BUFFER(FSceneTextureUniformParameters, SceneTexturesStruct)
+        SHADER_PARAMETER(float,  SharpStrength)
+        SHADER_PARAMETER(int32,  DepthMaskEnable)
+        SHADER_PARAMETER(int32,  RmsMaskEnable)
+        SHADER_PARAMETER(int32,  SharpenMode)
+    END_SHADER_PARAMETER_STRUCT()
 };
